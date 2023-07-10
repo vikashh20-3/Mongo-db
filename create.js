635078460a05c2a556931045b87219ed8c9009e0
vikashh20-3 <vvikashrao203@gmail.com>
2023-07-10 12:51:15 +0530

    create updated

--- create.js
+++ create.js
@@ -1,6 +1,34 @@
-db.demo.insertOne;
-({
+//for Node.js
+await db.collection("inventory").insertOne({
+  item: "canvas",
+  qty: 100,
+  tags: ["cotton"],
+  size: { h: 28, w: 35.5, uom: "cm" },
+});
+//for python
+db.demo.insertOne({
   item: "canvas",
   qty: 100,
   tags: ["Mechanical", "Simple"],
 });
+
+db.demo.insertMany([
+  {
+    item: "one",
+    qty: 20,
+    tags: ["demoone", "demotwo", "demothree"],
+    size: { h: 2, w: 4, suf: "cms" },
+  },
+  {
+    item: "two",
+    qty: 30,
+    tags: ["demoone", "demotwo", "demothree"],
+    size: { h: 2, w: 4, suf: "cms" },
+  },
+  {
+    item: "three",
+    qty: 40,
+    tags: ["demoone", "demotwo", "demothree"],
+    size: { h: 2, w: 4, suf: "cms" },
+  },
+]);
